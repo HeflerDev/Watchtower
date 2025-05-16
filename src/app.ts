e5e01c97ab5ba494d6625b78330d8e3117b36c9d
Hefler <heflerdev@gmail.com>
2025-05-16 15:25:28 -0300

    use app

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ moduleAlias.addAlias("@", __dirname);
 moduleAlias();
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import router from "./router";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(router);
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
