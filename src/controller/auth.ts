b431890151f287caadb085b8b8a0317726872b63
Hefler <heflerdev@gmail.com>
2025-05-16 18:59:23 -0300

    ADjust get

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,16 +1,33 @@
 import { Request, Response } from "express";
 import { apiKeys, generateApiKey } from "@/helper/apiKey";
-import { create } from "@/model/ApiKeyConnection";
+import * as apiKeyConnection from "@/model/ApiKeyConnection";
 
 export const post = async (req: Request, res: Response) => {
   const { host, username, privateKey } = req.body;
   const apiKey = generateApiKey();
 
-  const data = await create({ host, username, privateKey, apiKey });
+  const data = await apiKeyConnection.create({
+    host,
+    username,
+    privateKey,
+    apiKey,
+  });
 
   return res.status(200).send({ data });
 };
 
-export const get = (_req: Request, res: Response) => {
-  return res.status(200).json({ keys: Array.from(apiKeys) });
+export const get = async (req: Request, res: Response) => {
+  try {
+    const apiKey = req.headers["x-api-key"];
+
+    if (!apiKey || typeof apiKey !== "string") {
+      return res.status(401).json({ error: "API key is missing or invalid" });
+    }
+
+    const data = await apiKeyConnection.get(apiKey);
+
+    return res.status(200).send({ data });
+  } catch (error) {
+    return res.status(401).send({ error });
+  }
 };
