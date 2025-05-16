2ba0bfa4a30f7099b2c4e33cec2242f672be5571
Hefler <heflerdev@gmail.com>
2025-05-16 15:23:26 -0300

    Make apikey memory persistence

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,10 @@
-import { Request, Response, NextFunction, RequestHandler } from "express";
+import { RequestHandler } from "express";
+import { apiKeys } from "@/helper";
 
 export const verify: RequestHandler = (req, res, next) => {
   const apiKey = req.get("x-api-key");
 
-  if (!apiKey || apiKey !== process.env.MASTER_API_KEY) {
+  if (!apiKey || !apiKeys.has(apiKey)) {
     res.status(401).send({ error: "Unauthorized: Invalid API key" });
     return;
   }
