8043b7d782b8452c5d04c75b363913aa99c2eb54
Hefler <heflerdev@gmail.com>
2025-05-16 22:51:06 -0300

    Adjust app

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,13 @@ moduleAlias.addAlias("@", __dirname);
 moduleAlias();
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import router from "./router";
 import { connectToMongoose } from "./helper/mongoose";
 
-dotenv.config();
-
 const app: Express = express();
 
-app.use(router);
+app.use("/api", router);
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
