081339a927c6d05385c023bc91650b89245a1d1d
Hefler <heflerdev@gmail.com>
2025-05-17 19:37:44 -0300

    Add middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,15 +1,26 @@
-import { RequestHandler } from "express";
-import { apiKeys } from "@/helper";
+import { NextFunction, RequestHandler, Request, Response } from "express";
+import { get } from "@/model/ApiKeyConnection";
 
-export const verify: RequestHandler = async (req, res, next) => {
+interface RequestWithKey extends Request {
+  key?: string;
+  id?: string;
+}
+
+export const verify: RequestHandler = async (
+  req: RequestWithKey,
+  res: Response,
+  next: NextFunction,
+) => {
   const apiKey = req.get("x-api-key");
 
-  const check = await apiKeys(apiKey);
+  const keyDoc = await get(apiKey);
 
-  if (!apiKey || !check.includes(apiKey)) {
+  if (!apiKey || !keyDoc) {
     res.status(401).send({ error: "Unauthorized: Invalid API key" });
     return;
   }
 
+  req.key = keyDoc.apiKey;
+  req.id = keyDoc._id.toString();
   next();
 };
