801711b34fd1490fd19a2091c6b026e1ae29b4fb
Hefler <heflerdev@gmail.com>
2025-05-17 18:08:56 -0300

    Add master key

--- src/helper/apiKey.ts
+++ src/helper/apiKey.ts
@@ -2,6 +2,8 @@ import crypto from "crypto";
 import { get } from "@/model/ApiKeyConnection";
 
 const apiKeys = async (apiKey: string) => {
+  if (apiKey === process.env.MASTER_API_KEY)
+    return [process.env.MASTER_API_KEY];
   const data = await get(apiKey);
   return [data.apiKey, process.env.MASTER_API_KEY];
 };
