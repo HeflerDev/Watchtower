8bb102aed380e5262bcdde672c4f06fae19574be
Hefler <heflerdev@gmail.com>
2025-05-19 21:51:44 -0300

    SHip

--- src/controller/cmdLib.ts
+++ src/controller/cmdLib.ts
@@ -1,11 +1,22 @@
 import { Response } from "express";
-import { create } from "@/model/CmdLib";
+import * as CmdLib from "@/model/CmdLib";
 
 export const post = {
   new: async (req, res: Response) => {
     try {
       const { command, label } = req.body;
-      const data = await create(req.id, { command, label });
+      const data = await CmdLib.create(req.id, { command, label });
+      return res.status(200).send({ data });
+    } catch (e: any) {
+      return res.status(400).send({ message: e.message });
+    }
+  },
+};
+
+export const get = {
+  all: async (req, res: Response) => {
+    try {
+      const data = await CmdLib.get(req.id);
       return res.status(200).send({ data });
     } catch (e: any) {
       return res.status(400).send({ message: e.message });
